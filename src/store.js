f0e317cd384eacbc52dd66bcdd9aaa5da0f6d075
[Grace Kim] <gracekimm247@gmail.com>
2024-08-25 00:36:14 -0400

    remove pre-built-in todo list

--- src/store.js
+++ src/store.js
@@ -1,12 +1,9 @@
-// src/app/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/apiSlice";
-import todosReducer from "./features/api/todoslice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    todos: todosReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
